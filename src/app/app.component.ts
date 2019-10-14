dd8913e82d24dc5a35c0edb5ef928987ba8fb0b7
ahmed <ahmedgh@rosafian.com>
2019-10-14 22:00:01 +0100

    add precommit + prettify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { APP_CONFIG } from './app.config';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { APP_CONFIG } from "./app.config";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'structure';
+  title = "structure";
 
-  constructor(private http: HttpClient){
-      this.http.get(`${APP_CONFIG.apiBaseUrl}/users`).subscribe(
-        users=> console.log(`users => ${users}`)
-      )
+  constructor(private http: HttpClient) {
+    this.http
+      .get(`${APP_CONFIG.apiBaseUrl}/users`)
+      .subscribe(users => console.log(`users => ${users}`));
   }
 }
